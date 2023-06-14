1e6b61847d704600bc3268406d35960f1033ba04
Your Name~ <you@example.com>
2023-06-14 08:36:31 +0700

    session_8

---
+++ session_08/class/main.js
@@ -0,0 +1,50 @@
+console.log(` javascript external`);
+
+alert ("javascript");
+
+confirm(" co muon hoc hay ko ");
+
+prompt("nhap thong tin vao day ");
+
+let nameInfo = prompt("nhap ten");
+console.log(nameInfo);
+
+// = gan gia tri chi bien
+//^ 23 kieu du lieu 
+var age = 23;
+console.log(`tuoi cua toi ${age}`);
+
+var yourFood  = prompt (` cac mon an yeu thich cua ban`);
+var yourPlace = prompt (`dia diem du lich ua thich cua ban `);
+
+alert(`mon an ua thich cua ban ${yourFood} va dia diem du lich cua ban ${yourPlace}`);
+
+const lopHoc = "js230529";
+console.log(`khai bao bien ${lopHoc}` );
+
+lopHoc = "js230629";
+console.log(`khai bao bien ${lopHoc}` );
+
+// kiểu dữ liệu 
+// string => một đoạn chuỗi đc bọ dáu nháy đơn hoặc đấu huyền
+
+let nameTest = `npt`
+console.log(`kieu dữ liệu  ${nameTest}`);
+typeof(nameTest);
+// +promt => chuyển về string
+
+let number1 = 9;
+let number2 = 17;
+let result = number1 + number2;
+let div = (number1%2) + (number2%2);
+alert(`so su ${div}` );
+
+// prompt
+
+let yearBirth = parseInt(prompt('Year Birth'));
+let currentYear = parseInt(prompt('Current Year'));
+
+let total = currentYear - yearBirth ;
+alert(`Total: ${total}`);
+
+// bài tập 
